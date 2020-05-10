76cedd80ff9644cd6f026a2359601bac049fdfec
Chano Kim <ckim@expedia.com>
2020-05-10 20:31:20 +0100

    added comment and polished up the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,32 @@
+/*
+This is simple C code to demonstrate how to use interrupt and data register and data direction
+on low level without using Arduino functions. Performance boost from this is minimal but could be
+useful in some cases.
+
+*/
 #include <Arduino.h>
 
 #define LED 4
 #define BTN 2
 
-ISR(INT0_vect) {
-  PORTB ^= (1 << LED);
+ISR(INT0_vect) { // ISR
+  PORTB ^= (1 << LED); // flip over Port B pin 4 value
 }
 
 void setup() {
-  Serial.begin(9600);
-  DDRB |= _BV(LED);
+  DDRB |= _BV(LED); // Set data direction of PORT B 4(LED) to 1 (output)
 
-  DDRD |= _BV(BTN);
-  PORTD |= _BV(BTN);
+  DDRD |= _BV(BTN); // Set data direction of PORT D 2(BTN) to 1 (output)
+  PORTD |= _BV(BTN); // Set PORT D 2(BTN) to 1 (pull up register)
 
-  DDRB |= _BV(LED_BUILTIN);
-  PORTB &= ~_BV(LED_BUILTIN);
+  EICRA |= (1 << ISC01); // set bit1 of External Interrupt Control Register A to 1
+  EICRA &= ~(1 << ISC00); // set bit0 of External Interrupt Control Register A to 0
   
-  EICRA |= (1 << ISC01);
-  EICRA &= ~(1 << ISC00);
-  
-  EIMSK |= (1 << INT0);
-  sei();
+  EIMSK |= (1 << INT0); //External Interrupt Mask Register bit0 to 1
+  sei(); // Enable interrupts globally
 }
 
 void loop() {
-  delay(500);
+  delay(500); // do other shit.
 }
 
